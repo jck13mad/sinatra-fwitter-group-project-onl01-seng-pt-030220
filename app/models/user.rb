9f64ac27d4b2474ef0a9e99c95f4ff4bfb8c2983
Jack Madden <jck13mad@gmail.com>
2020-10-02 20:27:27 +0000

    Automatically backed up by Learn

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,14 @@
 class User < ActiveRecord::Base
-  has_secure_password
-  has_many :tweets
-end
+    has_many :tweets
+    has_secure_password
+  
+    def slug
+      username.gsub(" ", "-")
+    end
+  
+    def self.find_by_slug(slug)
+      self.all.find{ |username| username.slug }
+    end
+
+ 
+  end
